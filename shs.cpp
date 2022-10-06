487f43ce455c082b0677cef7fa1a3c71e7e35f6d
Colin S <colin6442@gmail.com>
2022-10-05 19:07:16 -0700

    some reorganization

--- shs.cpp
+++ shs.cpp
@@ -17,6 +17,52 @@
 
 using namespace cv;
 
+Mat getMat(HWND hWND) {
+
+	HDC deviceContext = GetDC(hWND);
+	HDC memoryDeviceContext = CreateCompatibleDC(deviceContext);
+
+	RECT windowRect;
+	GetClientRect(hWND, &windowRect);
+
+	int height = windowRect.bottom;
+	int width = windowRect.right;
+
+	HBITMAP bitmap = CreateCompatibleBitmap(deviceContext, width, height);
+
+	SelectObject(memoryDeviceContext, bitmap);
+
+	//copy data into bitmap
+	BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
+
+
+	//specify format by using bitmapinfoheader!
+	BITMAPINFOHEADER bi;
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = width;
+	bi.biHeight = -height;
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0; //because no compression
+	bi.biXPelsPerMeter = 1; //we
+	bi.biYPelsPerMeter = 2; //we
+	bi.biClrUsed = 3; //we ^^
+	bi.biClrImportant = 4; //still we
+
+	Mat mat = Mat(height, width, CV_8UC4); // 8 bit unsigned ints 4 Channels -> RGBA
+
+	//transform data and store into mat.data
+	GetDIBits(memoryDeviceContext, bitmap, 0, height, mat.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+	//clean up!
+	DeleteObject(bitmap);
+	DeleteDC(memoryDeviceContext); //delete not release!
+	ReleaseDC(hWND, deviceContext);
+
+	return mat;
+}
+
 struct MeanShiftPoint {
 	std::vector<Point> containedPoints;
 	bool hasPoints;
@@ -81,52 +127,6 @@ struct MeanShiftPoint {
 
 };
 
-Mat getMat(HWND hWND) {
-
-	HDC deviceContext = GetDC(hWND);
-	HDC memoryDeviceContext = CreateCompatibleDC(deviceContext);
-
-	RECT windowRect;
-	GetClientRect(hWND, &windowRect);
-
-	int height = windowRect.bottom;
-	int width = windowRect.right;
-
-	HBITMAP bitmap = CreateCompatibleBitmap(deviceContext, width, height);
-
-	SelectObject(memoryDeviceContext, bitmap);
-
-	//copy data into bitmap
-	BitBlt(memoryDeviceContext, 0, 0, width, height, deviceContext, 0, 0, SRCCOPY);
-
-
-	//specify format by using bitmapinfoheader!
-	BITMAPINFOHEADER bi;
-	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = width;
-	bi.biHeight = -height;
-	bi.biPlanes = 1;
-	bi.biBitCount = 32;
-	bi.biCompression = BI_RGB;
-	bi.biSizeImage = 0; //because no compression
-	bi.biXPelsPerMeter = 1; //we
-	bi.biYPelsPerMeter = 2; //we
-	bi.biClrUsed = 3; //we ^^
-	bi.biClrImportant = 4; //still we
-
-	Mat mat = Mat(height, width, CV_8UC4); // 8 bit unsigned ints 4 Channels -> RGBA
-
-	//transform data and store into mat.data
-	GetDIBits(memoryDeviceContext, bitmap, 0, height, mat.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
-
-	//clean up!
-	DeleteObject(bitmap);
-	DeleteDC(memoryDeviceContext); //delete not release!
-	ReleaseDC(hWND, deviceContext);
-
-	return mat;
-}
-
 bool detectPlayer(std::vector<MeanShiftPoint>& allPossible, Mat& empty, Point pos, int width, int height) {
 	double fromCenter = sqrt(pow(pos.x - width*.5, 2) + pow(pos.y - height*.5, 2)); // distance from center of game/screen
 	if (fromCenter > 85 && fromCenter < 120){
@@ -140,6 +140,22 @@ bool detectPlayer(std::vector<MeanShiftPoint>& allPossible, Mat& empty, Point po
 		return true;
 	}
 	return false;
+}
+
+struct Wall{
+	int distToCenter;
+	double slope;
+	int point1;
+	int point2;
+	double length;
+
+	Wall(int pt1, int pt2, double len){
+
+	}
+};
+
+void detectWalls(std::vector<Wall>& allWalls, Point pt1, Point pt2, Point middle, int width, int height) {
+	double fromCenter = sqrt(pow(middle.x - width*.5, 2) + pow(middle.y - height*.5, 2));
 }
 
 int main() {
@@ -183,25 +199,29 @@ int main() {
 		input = *(new MeanShiftPoint(width*.5 - 105*sqrt(2)/2, height*.5 - 105*sqrt(2)/2, 41));
 		allPossible.push_back(input);
 
+
 		// circle(empty, Point(width*.5, height*.5), 85, Scalar(255, 0, 0), 1, 8, 0);
 		// circle(empty, Point(width*.5, height*.5), 120, Scalar(255, 0, 0), 1, 8, 0);
 		// circle(empty, Point(width*.5, height*.5), 1, Scalar(255, 0, 0), 1, 8, 0);
 
-
-
-		int length;
+		double length;
 		bool pointDetected = false;
+		std::vector<Wall> allWalls;
+
 		for(size_t i = 0; i < lines.size(); i++) {
 			Vec4i l = lines[i];
 			length = sqrt(pow((double)(l[0] - l[2]), 2) + pow((double)(l[1] - l[3]), 2));
 			Point pos = Point(abs(l[0] + l[2]) / 2, abs(l[1] + l[3]) / 2);
 			// std::printf("in: %d, %d    out: %d, %d\n", x, y, pos.x, pos.y);
-			circle(empty, pos, 4, Scalar(255, 255, 0), 6, 8, 0);
 			line(empty, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 1, LINE_AA);
-			if (length < 20){
+			if(length < 20){
 				if(detectPlayer(allPossible, empty, pos, width, height)){
 					pointDetected = true;
 				}
+			}
+			if(pos.y > 70 && sqrt(pow(pos.x - width*.5, 2) + pow(pos.y - height*.5, 2)) > 120){
+				detectWalls(allWalls, Point(l[0], l[1]), Point(l[2], l[3]), pos, width, height);
+				// circle(empty, pos, 4, Scalar(255, 255, 0), 6, 8, 0);
 			}
 		}
 			
